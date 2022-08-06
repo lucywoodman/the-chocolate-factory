97d0c764c168c2373b223448214eaadf2d3ec47a
Lucy Woodman <lucyjwoodman@gmail.com>
2022-08-06 09:05:13 +0100

    chore: add url/view for product detail pages

--- products/urls.py
+++ products/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.full_range_products, name="products")]
+urlpatterns = [
+    path("", views.full_range_products, name="products"),
+    path("<product_id>", views.product_detail, name="product_detail"),
+]
