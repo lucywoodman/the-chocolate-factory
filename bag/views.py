56adb14951f6518fbca6081009c4873e56910594
Lucy Woodman <lucyjwoodman@gmail.com>
2022-08-20 20:18:08 +0100

    feat: add success message when adding items to cart to test toasts

--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from products.models import Product
 
 
 def view_bag(request):
@@ -10,14 +12,19 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """Add a qty of a specific item to the bag"""
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get("quantity"))
     redirect_url = request.POST.get("redirect_url")
     bag = request.session.get("bag", {})
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(
+            request, f"Added an extra {product.name} to your bag."
+        )
     else:
         bag[item_id] = quantity
+        messages.success(request, f"Added {product.name} to your bag.")
 
     request.session["bag"] = bag
     return redirect(redirect_url)
