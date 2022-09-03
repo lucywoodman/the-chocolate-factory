123da23e83f27b400d453f3d3b003580e3aad766
Lucy Woodman <lucyjwoodman@gmail.com>
2022-09-03 08:44:17 +0100

    feat: setup views + urls for checkout page to allow users to checkout

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from checkout.forms import OrderDetailForm
+
+
+def checkout(request):
+    bag = request.session.get("bag", {})
+    if not bag:
+        messages.error(request, "Looks like your bag is empty.")
+        return redirect(reverse("products"))
+
+    order_form = OrderDetailForm()
+    template = "checkout/checkout.html"
+    context = {
+        "order_form": order_form,
+    }
+
+    return render(request, template, context)
