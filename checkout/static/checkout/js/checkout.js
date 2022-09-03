3ea1a1a21fd982c8d7867c611ab451d7ea94b817
Lucy Woodman <lucyjwoodman@gmail.com>
2022-09-03 13:34:23 +0100

    feat: add error handling for Stripe form to provide customer feedback

--- checkout/static/checkout/js/checkout.js
+++ checkout/static/checkout/js/checkout.js
@@ -1,9 +1,11 @@
-/*
-    Core logic/payment flow for this comes from here:
-    https://stripe.com/docs/payments/accept-a-payment
-
-    CSS from here: 
-    https://stripe.com/docs/stripe-js
+/**
+* @fileOverview JavaScript for Stripe forms on The Chocolate Factory
+*
+* Core logic/payment flow comes from:
+* https://stripe.com/docs/payments/accept-a-payment
+*
+* CSS from here:
+* https://stripe.com/docs/stripe-js
 */
 
 let stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
@@ -28,3 +30,14 @@ let style = {
 };
 let card = elements.create('card', {style: style});
 card.mount('#card-element');
+
+// Handle realtime errors in Stripe form
+card.addEventListener('change', function(e) {
+    let errorDiv = document.getElementById('card-errors');
+    if (e.error) {
+        let html = `<i class="icon fa-solid fa-xmark"></i> ${e.error.message}`;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
