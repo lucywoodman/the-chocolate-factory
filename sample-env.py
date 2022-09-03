dec594f2dabf4cb28a11aec532297f23b349e994
Lucy Woodman <lucyjwoodman@gmail.com>
2022-09-03 14:07:25 +0100

    refac: move Stripe keys to env.py for more security/better management

--- sample-env.py
+++ sample-env.py
@@ -1,5 +1,7 @@
 import os
 
 
-os.environ['SECRET_KEY'] = ''
-os.environ['DEVELOPMENT'] = 'True'
+os.environ["SECRET_KEY"] = ""
+os.environ["DEVELOPMENT"] = "True"
+os.environ["STRIPE_PUBLIC_KEY"] = ""
+os.environ["STRIPE_SECRET_KEY"] = ""
