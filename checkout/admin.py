21e813b2beced4d72a31244d84e19c98828a1c3b
Lucy Woodman <lucyjwoodman@gmail.com>
2022-09-03 10:49:27 +0100

    feat: set up django admin with the checkout model information

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
+from .models import OrderDetail, OrderItem
 
-# Register your models here.
+
+class OrderItemAdmin(admin.TabularInline):
+    model = OrderItem
+    readonly_fields = ("item_total",)
+
+
+class OrderDetailAdmin(admin.ModelAdmin):
+    inlines = (OrderItemAdmin,)
+    readonly_fields = (
+        "order_number",
+        "date",
+        "delivery_cost",
+        "order_total",
+        "grand_total",
+    )
+
+    fields = (
+        "order_number",
+        "date",
+        "full_name",
+        "email",
+        "phone_number",
+        "street_address1",
+        "street_address2",
+        "town_or_city",
+        "county",
+        "postcode",
+        "country",
+        "delivery_cost",
+        "order_total",
+        "grand_total",
+    )
+
+    list_display = (
+        "order_number",
+        "date",
+        "full_name",
+        "order_total",
+        "delivery_cost",
+        "grand_total",
+    )
+
+    ordering = ("-date",)
+
+
+admin.site.register(OrderDetail, OrderDetailAdmin)
