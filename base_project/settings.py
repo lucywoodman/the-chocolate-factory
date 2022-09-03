0be3ef7abcef8e1e7de2847dd602ba8e5b00b8a4
Lucy Woodman <lucyjwoodman@gmail.com>
2022-09-03 08:43:31 +0100

    chore: install crispy forms + add to installed apps

--- base_project/settings.py
+++ base_project/settings.py
@@ -48,6 +48,8 @@ INSTALLED_APPS = [
     "allauth",
     "allauth.account",
     "allauth.socialaccount",
+    "crispy_forms",
+    "crispy_bootstrap5",
     # apps
     "home",
     "products",
@@ -68,6 +70,9 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "base_project.urls"
 
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
@@ -83,6 +88,10 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
                 "bag.contexts.bag_contents",
+            ],
+            "builtins": [
+                "crispy_forms.templatetags.crispy_forms_tags",
+                "crispy_forms.templatetags.crispy_forms_field",
             ],
         },
     },
