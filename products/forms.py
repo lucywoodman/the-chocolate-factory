082ac73b30761d9ba1a6237710840cc1aeee1e4c
Lucy Woodman <lucyjwoodman@gmail.com>
2022-09-04 17:06:46 +0100

    feat: create form for shop products to allow superusers to add/update products

---
+++ products/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Product, Category
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs[
+                "class"
+            ] = "border border-dark rounded-0"
