2052aa65e7a55e5e3d4ecb7023aca959892db9be
Lucy Woodman <lucyjwoodman@gmail.com>
2022-09-04 10:13:51 +0100

    feat: update profile page to include new form + tweak styles

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from .models import Profile
+from .forms import ProfileForm
 
 
 def profile(request):
+    profile = get_object_or_404(Profile, user=request.user)
+
+    if request.method == "POST":
+        form = ProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Profile updated successfully!")
+
+    form = ProfileForm(instance=profile)
+    orders = profile.orders.all()
+
     template = "profiles/profile.html"
-    context = {}
+    context = {
+        "profile": profile,
+        "form": form,
+        "orders": orders,
+    }
 
     return render(request, template, context)
