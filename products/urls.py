884bfb406f9a6d52e8ca2ae609d22364db2a6895
Lucy Woodman <lucyjwoodman@gmail.com>
2022-09-04 17:57:39 +0100

    feat: add view+url to update existing products to allow users to manage content

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path("", views.FullProductRange.as_view(), name="products"),
     path("add/", views.add_product, name="add_product"),
+    path(
+        "update/<int:product_id>", views.update_product, name="update_product"
+    ),
     path("<slug:slug>/", views.ProductDetail.as_view(), name="product_detail"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, reverse, redirect
+from django.shortcuts import render, reverse, redirect, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from .models import Product, Category
@@ -70,6 +70,31 @@ def add_product(request):
     template = "products/add_product.html"
     context = {
         "form": form,
+    }
+
+    return render(request, template, context)
+
+
+def update_product(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == "POST":
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Successfully updated product!")
+            return redirect(reverse("product_detail", args=[product.slug]))
+        else:
+            messages.error(
+                request,
+                "Oops, something went wrong! Please double-check the form.",
+            )
+    else:
+        form = ProductForm(instance=product)
+
+    template = "products/update_product.html"
+    context = {
+        "form": form,
+        "product": product,
     }
 
     return render(request, template, context)
