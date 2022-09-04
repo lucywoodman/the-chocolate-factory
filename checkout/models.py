50ce1e7d00a0dd7928e9b3c815f91ad8d9e70adf
Lucy Woodman <lucyjwoodman@gmail.com>
2022-09-04 07:10:46 +0100

    feat: update checkout model to connect to user profiles

--- checkout/models.py
+++ checkout/models.py
@@ -1,10 +1,11 @@
+import uuid
 from django.db import models
-from products.models import Product
 from django.db.models import Sum
 from django.conf import settings
 from django_countries.fields import CountryField
 
-import uuid
+from products.models import Product
+from profiles.models import Profile
 
 
 class OrderDetail(models.Model):
@@ -12,15 +13,22 @@ class OrderDetail(models.Model):
         verbose_name = "Order"
 
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    profile = models.ForeignKey(
+        Profile,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="orders",
+    )
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=256, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
-    country = CountryField(blank_label="Country *", null=False, blank=False)
-    postcode = models.CharField(max_length=20, null=False, blank=False)
-    town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
     county = models.CharField(max_length=80, null=True, blank=True)
+    postcode = models.CharField(max_length=20, null=False, blank=False)
+    country = CountryField(blank_label="Country *", null=False, blank=False)
     date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(
         max_digits=6, decimal_places=2, null=False, default=0
