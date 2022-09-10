abca7dd97229237addb73c5b221a57c8b06ffac7
Lucy Woodman <lucyjwoodman@gmail.com>
2022-09-10 10:30:37 +0100

    feat: create form + template for adding new producers to the database

---
+++ producers/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Producer
+
+
+class ProducerForm(forms.ModelForm):
+    class Meta:
+        model = Producer
+        exclude = ("photo_url",)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs[
+                "class"
+            ] = "border border-dark rounded-0"
