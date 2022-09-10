6ba9180ca6173f14f991cb50e9141befc2b23b1c
Lucy Woodman <lucyjwoodman@gmail.com>
2022-09-10 09:17:50 +0100

    refac: update view to use CBV as its a simple list view

--- producers/views.py
+++ producers/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.views import generic
+from .models import Producer
 
 
-def producers(request):
-    """A view to return the producers page"""
-    return render(request, "producers/producers.html")
+class Producers(generic.ListView):
+    """A view to return the list of producers"""
+
+    model = Producer
+    context_object_name = "producers"
+    template_name = "producers/producers.html"
