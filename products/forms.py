4bc54c854b95c71a11d768acb2aa55d257f10c4f
Lucy Woodman <lucyjwoodman@gmail.com>
2022-09-10 07:41:48 +0100

    feat: reorder form fields + add checkboxes for allergies to make them easier to select

--- products/forms.py
+++ products/forms.py
@@ -5,9 +5,22 @@ from .models import Product
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        exclude = [
-            "slug",
+        fields = [
+            "name",
+            "producer",
+            "price",
+            "weight",
+            "type",
+            "category",
+            "flavour",
+            "allergy_info",
+            "details",
+            "ingredients",
+            "image",
         ]
+        widgets = {
+            "allergy_info": forms.CheckboxSelectMultiple,
+        }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
