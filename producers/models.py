9f9fcf8907eaec5a8aef4c2777e1d805f543a693
Lucy Woodman <lucyjwoodman@gmail.com>
2022-09-10 09:18:36 +0100

    feat: add photo field to producers so we can easily display them in the template

--- producers/models.py
+++ producers/models.py
@@ -7,6 +7,8 @@ class Producer(models.Model):
     name = models.CharField(max_length=256)
     location = models.CharField(max_length=256, blank=True, null=True)
     details = models.TextField(blank=True, null=True)
+    photo_url = models.URLField(max_length=1024, blank=True, null=True)
+    photo = models.ImageField(blank=True, null=True)
 
     def __str__(self):
         return self.name
