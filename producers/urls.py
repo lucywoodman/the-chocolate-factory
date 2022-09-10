395e1e3e249b586ba6d1553d918c52a3899259ce
Lucy Woodman <lucyjwoodman@gmail.com>
2022-09-10 10:30:11 +0100

    feat: add url + view for adding new producers to the database

--- producers/urls.py
+++ producers/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.Producers.as_view(), name="producers"),
+    path("add/", views.AddProducer.as_view(), name="add_producer"),
 ]
--- producers/views.py
+++ producers/views.py
@@ -1,4 +1,8 @@
 from django.views import generic
+from django.urls import reverse_lazy
+from django.shortcuts import redirect
+from django.contrib.auth.mixins import UserPassesTestMixin
+from producers.forms import ProducerForm
 from .models import Producer
 
 
@@ -8,3 +12,17 @@ class Producers(generic.ListView):
     model = Producer
     context_object_name = "producers"
     template_name = "producers/producers.html"
+
+
+class AddProducer(UserPassesTestMixin, generic.CreateView):
+    """
+    A view to display the producer form.
+    To add new producers. Restricted to superusers only.
+    """
+
+    model = Producer
+    form_class = ProducerForm
+    success_url = reverse_lazy("producers")
+
+    def test_func(self):
+        return self.request.user.is_superuser
