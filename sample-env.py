d4f8d9aed8c55e9699c4358d352a6fbba7483163
Lucy Woodman <lucyjwoodman@gmail.com>
2022-09-11 13:19:07 +0100

    feat: update sample env.py with mailchimp variables

--- sample-env.py
+++ sample-env.py
@@ -1,7 +1,9 @@
 import os
 
-
 os.environ["SECRET_KEY"] = ""
 os.environ["DEVELOPMENT"] = "True"
 os.environ["STRIPE_PUBLIC_KEY"] = ""
 os.environ["STRIPE_SECRET_KEY"] = ""
+os.environ["MAILCHIMP_API_KEY"] = ""
+os.environ["MAILCHIMP_DATA_CENTER"] = ""
+os.environ["MAILCHIMP_EMAIL_LIST_ID"] = ""
