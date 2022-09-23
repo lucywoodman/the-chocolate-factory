9b3badf6c560fbab648d885e013ba0da452f443c
Lucy Woodman <lucyjwoodman@gmail.com>
2022-09-23 18:29:40 +0100

    feat: add success messages and no perm handling for better user feedback

--- producers/views.py
+++ producers/views.py
@@ -1,6 +1,9 @@
+from django.shortcuts import redirect
+from django.contrib import messages
 from django.views import generic
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import UserPassesTestMixin
+from django.contrib.messages.views import SuccessMessageMixin
 from producers.forms import ProducerForm
 from .models import Producer
 
@@ -13,7 +16,9 @@ class Producers(generic.ListView):
     template_name = "producers/producers.html"
 
 
-class AddProducer(UserPassesTestMixin, generic.CreateView):
+class AddProducer(
+    UserPassesTestMixin, SuccessMessageMixin, generic.CreateView
+):
     """
     A view to display the producer form.
     To add new producers. Restricted to superusers only.
@@ -21,13 +26,22 @@ class AddProducer(UserPassesTestMixin, generic.CreateView):
 
     model = Producer
     form_class = ProducerForm
+    success_message = "Successfully added producer!"
     success_url = reverse_lazy("producers")
 
     def test_func(self):
         return self.request.user.is_superuser
 
+    def handle_no_permission(self):
+        messages.error(
+            self.request, "You need proper authorisation to do that."
+        )
+        return redirect("home")
 
-class UpdateProducer(UserPassesTestMixin, generic.UpdateView):
+
+class UpdateProducer(
+    UserPassesTestMixin, SuccessMessageMixin, generic.UpdateView
+):
     """
     A view to display the producer form.
     To update producers. Restricted to superusers only.
@@ -35,13 +49,22 @@ class UpdateProducer(UserPassesTestMixin, generic.UpdateView):
 
     model = Producer
     form_class = ProducerForm
+    success_message = "Successfully updated producer!"
     success_url = reverse_lazy("producers")
 
     def test_func(self):
         return self.request.user.is_superuser
 
+    def handle_no_permission(self):
+        messages.error(
+            self.request, "You need proper authorisation to do that."
+        )
+        return redirect("home")
 
-class DeleteProducer(UserPassesTestMixin, generic.DeleteView):
+
+class DeleteProducer(
+    UserPassesTestMixin, SuccessMessageMixin, generic.DeleteView
+):
     """
     A view to delete producers.
     Restricted to superusers only.
@@ -49,7 +72,14 @@ class DeleteProducer(UserPassesTestMixin, generic.DeleteView):
 
     model = Producer
     template = "producers/producers.html"
+    success_message = "Successfully deleted producer!"
     success_url = reverse_lazy("producers")
 
     def test_func(self):
         return self.request.user.is_superuser
+
+    def handle_no_permission(self):
+        messages.error(
+            self.request, "You need proper authorisation to do that."
+        )
+        return redirect("home")
