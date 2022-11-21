ba8785c293cfcc232e611d539a7cc22fe55794be
Lucy Woodman <lucy.woodman@wpengine.com>
2022-11-21 19:11:34 +0000

    build(settings.py): ensure render URL is included in allowed hosts

--- base_project/settings.py
+++ base_project/settings.py
@@ -34,6 +34,11 @@ DEBUG = development
 
 ALLOWED_HOSTS = [os.environ.get("HOST"), "localhost", "127.0.0.1"]
 
+# Add Render.com URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+   ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 # Application definition
 
